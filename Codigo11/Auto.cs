3b39bb7ae5f50eef1e8d980249d93d173c22889c
Adrian0107 <61296862+Adrian0107@users.noreply.github.com>
2020-02-26 10:46:56 -0600

    Add files via upload

---
+++ Codigo11/Auto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automovil
+{
+	class Auto
+	{
+		private double costoVehiculo;
+
+		public double CostoVehiculo
+		{
+			get { return costoVehiculo; }
+			set { costoVehiculo = value; }
+		}
+		private double gananciaVendedor;
+
+		public double GananciaVendedor
+		{
+			get { return gananciaVendedor; }
+			set { gananciaVendedor = value; }
+		}
+		private double impuestosAplicables;
+
+		public double ImpuestosAplicables
+		{
+			get { return impuestosAplicables; }
+			set { impuestosAplicables = value; }
+		}
+
+		
+
+
+
+	}
+}
---
+++ Codigo11/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Automovil
+{
+    public partial class Form1 : Form
+    {
+        Auto automovil = new Auto();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            automovil.CostoVehiculo = double.Parse(textBox1.Text);
+            textBox1.Clear();
+        }
+        public double CalcularCosto()
+        {
+            costo = automovil.CostoVehiculo;
+            return (costo+(costo*.12)+(Costo*.06));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"El costo total del Automovil es de: {CalcularCosto()}");
+        }
+    }
+    
+}
