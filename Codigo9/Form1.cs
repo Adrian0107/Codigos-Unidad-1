e5752b15be07c1e7418612daa7cc455e1ce49749
Adrian0107 <61296862+Adrian0107@users.noreply.github.com>
2020-02-26 10:45:17 -0600

    Add files via upload

---
+++ Codigo9/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gasolinera
+{
+    public partial class Form1 : Form
+    {
+        Gasolinera gas = new Gasolinera();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"El precio a cobrar sera de: {gas.PrecioEnGalones()}");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            gas.Galones = double.Parse(textBox1.Text);
+            textBox1.Clear();
+        }
+    }
+}
---
+++ Codigo9/Gasolinera.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gasolinera
+{
+    class Gasolinera
+    {
+        private int Precio;
+        public Gasolinera()
+        {
+            Console.WriteLine("Litro de gasolina: 15$ ");
+            Precio = 15;
+        }
+        private double galones; 
+
+        public double Galones 
+        {
+            get { return galones; }
+            set { galones = value; }
+        }
+        public double PrecioEnGalones()
+        {
+            return (Galones * 3.785)*Precio;
+        }
+
+
+    }
+}
