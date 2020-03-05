7e45dba9524e38c2b91b26469cb009592dd0dfb6
Adrian0107 <61296862+Adrian0107@users.noreply.github.com>
2020-03-04 20:10:08 -0600

    Add files via upload

---
+++ 15/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace llamada
+{
+    public partial class Form1 : Form
+    {
+        Llamada telefono = new Llamada();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           telefono.Duracion = int.Parse(textBox1.Text);
+           textBox1.Clear();
+            if (telefono.Duracion >= 0)
+            {
+                MessageBox.Show($"El costo de la llamada es: {telefono.CalcularCostoLlamada()}");
+            }
+            else
+            {
+                MessageBox.Show("Introducir un numero entero positivo");
+            }
+        }
+    }
+}
---
+++ 15/Llamada.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace llamada
+{
+    class Llamada
+    {
+
+		private int duracion;
+
+		public int Duracion
+		{
+			get { return duracion; }
+			set { duracion = value; }
+		}
+		public double CalcularCostoLlamada()
+		{
+			if (Duracion <= 3)
+			{
+				return 5;
+			}
+			else
+			{
+				return (Duracion -3)*3 + 5;
+			}
+		}
+	}
+}
