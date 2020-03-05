56c4ec988042210be1e39623a37d237f3c88d6f3
Adrian0107 <61296862+Adrian0107@users.noreply.github.com>
2020-03-04 19:51:33 -0600

    Add files via upload

---
+++ 13/tiempo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Segundos
+{
+    class tiempo
+    {
+		private int dias;
+
+		public int Dias
+		{
+			get { return dias; }
+			set { dias = value; }
+		}
+		private int segundos;
+
+		public int Segundos
+		{
+			get { return segundos; }
+			set { segundos = value; }
+		}
+
+		public int CalcularSegundos()
+		{
+			return Dias * 86400;
+		}
+
+
+	}
+}
